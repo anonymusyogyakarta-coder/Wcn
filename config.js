951929be1f2e16897f996f3512f549d2db28dee9
Zexr01 <anonymusyogyakarta@gmail.com>
2026-06-16 22:56:33 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    botName: "WCN PREMIUM BOT",
+    ownerName: "Anonymous Yogyakarta Coder",
+    ownerNumber: ["628123456789"], // Ganti pake nomor WA lo sendiri, Masbrok
+    prefix: ".",
+    
+    // API KEY SAKTI LO (Tersimpan Aman)
+    OPENAI_API_KEY: "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",
+    HF_WEATHER_KEY: "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",
+    CLIPDROP_GENERATE_KEY: "37cac78ea8cc34e95d945f160ba2c99a"
+};
